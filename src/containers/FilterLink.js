c82d75dac9bdbc5473cf3eaf5b1f2b840ccf7e42
Marvin Arcilla <arcilla.bumblebeen@gmail.com>
2018-01-17 13:46:34 +0800

    Use NavLink to navigate between routers

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -1,20 +1,37 @@
-import { connect } from 'react-redux'
-import { setVisibilityFilter } from '../actions'
-import Link from '../components/Link'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-})
+const FilterLink = ({filter, children}) => (
+  <NavLink 
+    to={filter === '/all' ? '' : '/' + filter}
+    activeStyle={{
+      textDecoration: 'none',
+      color: 'black',
+    }}
+  >
+    {children}
+  </NavLink>
+);
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => {
-    dispatch(setVisibilityFilter(ownProps.filter))
-  }
-})
+export default FilterLink;
 
-const FilterLink = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Link)
+// import { connect } from 'react-redux'
+// import { setVisibilityFilter } from '../actions'
+// import Link from '../components/Link'
 
-export default FilterLink
+// const mapStateToProps = (state, ownProps) => ({
+//   active: ownProps.filter === state.visibilityFilter
+// })
+
+// const mapDispatchToProps = (dispatch, ownProps) => ({
+//   onClick: () => {
+//     dispatch(setVisibilityFilter(ownProps.filter))
+//   }
+// })
+
+// const FilterLink = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Link)
+
+// export default FilterLink
